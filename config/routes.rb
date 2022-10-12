57e153e9b226731386e3c88652330d1390fd1760
isakashiori <isslamdunk0000@gmail.com>
2022-10-12 20:28:06 +0900

    管理画面にログインした以降の処理

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    email: "admin@example.com",
+    password: "password",
+    password_confirmation: "password",
+    first_name: "isaka",
+    last_name: "shiori",
+    avatar: "管理者",
+    role: 1
+)
+User.create!(
+    email: "user@example.com",
+    password: "password1",
+    password_confirmation: "password1",
+    first_name: "user",
+    last_name: "user",
+    avatar: "ユーザー",
+    role: 0
+)

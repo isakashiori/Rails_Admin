f29039118807acde2cc4042974f566f6a47f2663
isakashiori <isslamdunk0000@gmail.com>
2022-10-17 15:41:23 +0900

    fix:修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
                  else
                   'RUNTEQ BOARD APP'
                  end
-    page_title.empty? ? base_title : "#{page_title}  | "  base_title
+    page_title.empty? ? base_title : page_title + " | " +  base_title
   end
 end
